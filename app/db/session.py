a05299a13292749c16c739791072a993bf8ad870
Анна Теплова <ann4v4nn4@yandex.ru>
2026-03-09 06:32:54 +0300

    change to flexible with postgresql

--- app/db/session.py
+++ app/db/session.py
@@ -4,10 +4,14 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import DATABASE_URL
 from app.db.models import Base
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
-)
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+if DATABASE_URL and "sqlite" in DATABASE_URL:
+    engine = create_engine(
+        DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
